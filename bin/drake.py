cec76c3ce3d13ca0dba8dc296ce935f0247d0661
Ishan Manchanda <ishanmanchanda70@gmail.com>
2022-10-06 15:31:40 +0530

    Entirely refactored drake.py

--- bin/drake.py
+++ bin/drake.py
@@ -12,17 +12,20 @@ font_path = path_join(bin_path, 'resources/fonts/raleway.ttf')
 font = ImageFont.truetype(font_path, 32)
 
 
-def drake(update, top, bottom):
+def drake(update, top_text, bottom_text):
     meme_format = ('drake', 'drake', 'robbie', 'babushka')[randint(0, 3)]
     img_path = path_join(bin_path, 'resources/drake/', f'{meme_format}.png')
 
     bio = BytesIO()
     img = Image.open(img_path)
-    draw = ImageDraw.Draw(img)
+    draw_obj = ImageDraw.Draw(img)
 
-    # Top, bottom y coordinates
-    y1, y2 = 129, 387
-    if not (__draw_text(draw, top, y1) and __draw_text(draw, bottom, y2)):
+    # Manually identified top and bottom y coordinates
+    y_top, y_bottom = 129, 387
+    if not (
+            __draw_text(draw_obj, top_text, y_top) and
+            __draw_text(draw_obj, bottom_text, y_bottom)
+    ):
         log_error('Drake meme generation failed!')
         update.message.reply_photo(Files.drake, quote=True)
         return
@@ -34,9 +37,9 @@ def drake(update, top, bottom):
 
 
 def __get_lines(text):
-    w = font.getsize(text)[0]
+    text_width = font.getsize(text)[0]
     # Check if the entire block fits in a single line
-    if w <= 320:
+    if text_width <= 320:
         return [text]
 
     text = text.split()
@@ -44,43 +47,54 @@ def __get_lines(text):
     # that will fit in a single line.
     # After finding that, recurse to find the next line(s).
     for i in range(len(text), -1, -1):
-        segment = ' '.join(text[:i])
-        w = font.getsize(segment)[0]
+        chosen_prefix = ' '.join(text[:i])
+        text_width = font.getsize(chosen_prefix)[0]
 
-        if w <= 320:
-            remainder = ' '.join(text[i:])
-            return [segment] + __get_lines(remainder)
+        if text_width <= 320:
+            remaining_suffix = ' '.join(text[i:])
+            return [chosen_prefix] + __get_lines(remaining_suffix)
 
     # If even a single word doesn't fit, we give up and raise a ValueError
-    # TODO: Ideally should keep trying with smaller fonts
-    #  until we reach a practical readability limit
+    # REVIEW: Ideally should keep trying with smaller fonts
+    #  until we reach a practical readability limit?
     raise ValueError
 
 
-def __draw_text(draw, t, y):
-    # TODO: Make readable, fix param names
-    t = t.strip()
-    w, h = font.getsize(t)
+def __draw_text(draw_obj, text, y_coordinate):
+    text = text.strip()
+    text_width, text_height = font.getsize(text)
+    color = (255, 255, 255)
 
-    if w <= 320:
-        draw.text((480 - (w / 2), y - (h / 2)), t, (255, 255, 255), font=font)
+    # Check if text width within limit
+    if text_width <= 320:
+        x_center = 480 - (text_width / 2)
+        y_center = y_coordinate - (text_height / 2)
+        draw_obj.text((x_center, y_center), text, color, font=font)
         return True
 
+    # Else try multi-line
     try:
-        lines = __get_lines(t)
+        lines = __get_lines(text)
+    # Value error thrown if even a single word doesn't fit in a line
     except ValueError:
         return False
+
+    # Get the number of lines and dimensions of each
     num_lines = len(lines)
     dims = [font.getsize(x) for x in lines]
-    ws, hs = [x[0] for x in dims], [x[1] for x in dims]
-    total = sum(hs)
+    line_widths, line_heights = [x[0] for x in dims], [x[1] for x in dims]
 
-    if total > 258:
+    # Check if total height greater than acceptable
+    total_height = sum(line_heights)
+    if total_height > 258:
         return False
 
-    h = y - (total / 2)
+    # Compute y-coordinate for line 1
+    y_center = y_coordinate - (total_height / 2)
     for i in range(num_lines):
-        draw.text((480 - (ws[i] / 2), h), lines[i], (255, 255, 255), font=font)
-        h += hs[i]
+        x_center = 480 - (line_widths[i] / 2)
+        draw_obj.text((x_center, y_center), lines[i], color, font=font)
+        # Increment next y-coordinate by current line's height
+        y_center += line_heights[i]
 
     return True
